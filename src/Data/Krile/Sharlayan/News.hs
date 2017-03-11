603a07f94458e32efb4b018c2fa06fa869b5cb2b
Callum Mance <callummance@gmail.com>
2017-03-11 03:00:29 +0000

    Add missing Haddock documentation for News module

--- src/Data/Krile/Sharlayan/News.hs
+++ src/Data/Krile/Sharlayan/News.hs
@@ -1,22 +1,31 @@
+-- |Functions for scraping news data from the lodestone
 module Data.Krile.Sharlayan.News (fetchNews)
   where
 
 import Network.HTTP
 import Text.HTML.TagSoup
 
-
+-- |The page that needed to be fetched for news scraping
 lsNewsPage :: String
 lsNewsPage = "http://eu.finalfantasyxiv.com/lodestone/news"
 
+-- |Fetch a list of titles and links from the default news URL
 fetchNews :: IO [(String, String)]
-fetchNews = parseLodestone lsNewsPage
+fetchNews = parseLodestoneNews lsNewsPage
 
+
+
+
+
+
+-- |Helper function to fetch the contents of a URL
 openURL :: String -> IO String
 openURL url
   = getResponseBody =<< simpleHTTP (getRequest url)
 
-parseLodestone :: String -> IO [(String, String)]
-parseLodestone url
+-- |Parse the lodestone news page to generate a list of title-url tuples
+parseLodestoneNews :: String -> IO [(String, String)]
+parseLodestoneNews url
   = do
     tags <- parseTags <$> openURL url
     let entries = concat $ map (take 4) $ partitions findNewsEntries tags
