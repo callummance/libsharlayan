9000e9dc5028943d9ffea44c956bbd47a64ad7b0
Callum <callummance@gmail.com>
2017-03-21 06:17:30 +0000

    Add haddock documentation to TakeWhileM module

--- src/Control/Monad/TakeWhileM.hs
+++ src/Control/Monad/TakeWhileM.hs
@@ -1,8 +1,10 @@
+-- |Module containing a lazy monadic takeWhile function
 module Control.Monad.TakeWhileM
   where
 
 import Control.Monad
 
+-- |Given a perdicate and a list of monad elements, returns a list inside the monad of elements up until the first element which does not satisfy the predicate
 takeWhileM :: (Monad m) => (a -> Bool) -> [m a] -> m [a]
 takeWhileM f [] = return []
 takeWhileM f (x:xs)
